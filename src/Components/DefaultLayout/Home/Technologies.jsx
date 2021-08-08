9683288443d816035102fa080685738174dab18f
mostafizrr2 <mostafizrrahman2@gmail.com>
2021-08-08 23:12:58 +0600

    sd

--- src/Components/DefaultLayout/Home/Technologies.jsx
+++ src/Components/DefaultLayout/Home/Technologies.jsx
@@ -10,91 +10,91 @@ export default function Technologies() {
   
           <div className=" grid grid-cols-3 md:grid-cols-7 gap-3 md:gap-5 mt-5">
   
-              <a href="#" class="p-2 bg-white rounded-xl shadow-md hover:shadow-2xl transition duration-300">
+              <a href="#" class="p-2 border border-gray-500 bg-white rounded-xl shadow-md hover:shadow-xl transition duration-300">
                 <img class="rounded-xl" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSu0QQ15PkoyYoaolvXNeswgRryR3ZdZyMKsxmYgPSAlNVcMLR6d-JWbvnYLAUJ9kuMmwo&usqp=CAU" alt="" />
               </a>
   
-              <a href="#" class="p-2 bg-white rounded-xl shadow-md hover:shadow-2xl transition duration-300">
+              <a href="#" class="p-2 border border-gray-500 bg-white rounded-xl shadow-md hover:shadow-xl transition duration-300">
                 <img class="rounded-xl" src="https://devanshusehgal.github.io/portfolio/assets/tags/php.png" alt="" />
               </a>
   
-              <a href="#" class="p-2 bg-white rounded-xl shadow-md hover:shadow-2xl transition duration-300">
+              <a href="#" class="p-2 border border-gray-500 bg-white rounded-xl shadow-md hover:shadow-xl transition duration-300">
                 <img class="rounded-xl" src="https://discoverprograming.com/wp-content/uploads/2020/06/laravel-framework-logo-C10176EC8C-seeklogo.com_.png" alt="" />
               </a>
   
   
-              <a href="#" class="p-2 bg-white rounded-xl shadow-md hover:shadow-2xl transition duration-300">
+              <a href="#" class="p-2 border border-gray-500 bg-white rounded-xl shadow-md hover:shadow-xl transition duration-300">
                 <img class="rounded-xl" src="https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FEmft2%2FbtqF3ctJqOK%2FAIlRdVd0JDwhyCexGFGKW0%2Fimg.jpg" alt="" />
               </a>
   
-              <a href="#" class="p-2 bg-white rounded-xl shadow-md hover:shadow-2xl transition duration-300">
-                <img class="rounded-xl" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiABio3q4JJ_PuCKk3oV4o6J7Zz4Dlv1nA_w&usqp=CAU" alt="" />
+              <a href="#" class="p-2 border border-gray-500 bg-white rounded-xl shadow-md hover:shadow-xl transition duration-300">
+                <img class="rounded-xl" src="https://symbols.getvecta.com/stencil_73/34_adonisjs.afb87a2e32.jpg" alt="" />
               </a>
   
   
-              <a href="#" class="p-2 bg-white rounded-xl shadow-md hover:shadow-2xl transition duration-300">
+              <a href="#" class="p-2 border border-gray-500 bg-white rounded-xl shadow-md hover:shadow-xl transition duration-300">
                 <img class="rounded-xl" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR045MOPskiBF1v85HPVRs8ufbNb48Cd4v10nYdxMQQcVWDHALWheVdaNIa5OLt7qTNy1M&usqp=CAU" alt="" />
               </a>
   
-              <a href="#" class="p-2 bg-white rounded-xl shadow-md hover:shadow-2xl transition duration-300">
+              <a href="#" class="p-2 border border-gray-500 bg-white rounded-xl shadow-md hover:shadow-xl transition duration-300">
                 <img class="rounded-xl" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBQaiY5QtLFwHWHjjPZMGRQOeRUgcxLjLOXuDFOLY9yzoLGe-1I6bUpIiWtKv1t5zrpsc&usqp=CAU" alt="" />
               </a>
   
-              <a href="#" class="p-2 bg-white rounded-xl shadow-md hover:shadow-2xl transition duration-300">
+              <a href="#" class="p-2 border border-gray-500 bg-white rounded-xl shadow-md hover:shadow-xl transition duration-300">
                 <img class="rounded-xl" src="https://cdn.iconscout.com/icon/free/png-512/vuejs-1175052.png" alt="" />
               </a>
 
-              <a href="#" class="p-2 bg-white rounded-xl shadow-md hover:shadow-2xl transition duration-300">
+              <a href="#" class="p-2 border border-gray-500 bg-white rounded-xl shadow-md hover:shadow-xl transition duration-300">
                 <img class="rounded-xl" src="https://www.sovereignconsult.com/wp-content/uploads/2019/10/angular-js.png" alt="" />
               </a>
 
   
-              <a href="#" class="p-2 bg-white rounded-xl shadow-md hover:shadow-2xl transition duration-300">
+              <a href="#" class="p-2 border border-gray-500 bg-white rounded-xl shadow-md hover:shadow-xl transition duration-300">
                 <img class="rounded-xl" src="https://camo.githubusercontent.com/c8edb8c44c6d5779eb077924888a4f39f134339bea41d6b242a40a6755215552/687474703a2f2f696d6775722e636f6d2f56344c746f49492e706e67" alt="" />
               </a>
-              <a href="#" class="p-2 bg-white rounded-xl shadow-md hover:shadow-2xl transition duration-300">
+              <a href="#" class="p-2 border border-gray-500 bg-white rounded-xl shadow-md hover:shadow-xl transition duration-300">
                 <img class="rounded-xl" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS274fIzc235xWUHSvqPjfgQsQlO5hgLpfKygJ-qAwhI73YWbyloKkerf6Tg6of6Cgx1jo&usqp=CAU" alt="" />
               </a>
 
-              <a href="#" class="p-2 bg-white rounded-xl shadow-md hover:shadow-2xl transition duration-300">
+              <a href="#" class="p-2 border border-gray-500 bg-white rounded-xl shadow-md hover:shadow-xl transition duration-300">
                 <img class="rounded-xl" src="https://gocode.colorado.gov/wp-content/uploads/2020/11/MongoDB-logo.gif" alt="" />
               </a>
 
-              <a href="#" class="p-2 bg-white rounded-xl shadow-md hover:shadow-2xl transition duration-300">
+              <a href="#" class="p-2 border border-gray-500 bg-white rounded-xl shadow-md hover:shadow-xl transition duration-300">
                 <img class="rounded-xl" src="http://pngimg.com/uploads/mysql/small/mysql_PNG23.png" alt="" />
               </a>
 
-              <a href="#" class="p-2 bg-white rounded-xl shadow-md hover:shadow-2xl transition duration-300">
+              <a href="#" class="p-2 border border-gray-500 bg-white rounded-xl shadow-md hover:shadow-xl transition duration-300">
                 <img class="rounded-xl" src="https://kenoleon.github.io/Front-End-Web-Dev-UI-UX/assets/images/firebase.png" alt="" />
               </a>
 
-              <a href="#" class="p-2 bg-white rounded-xl shadow-md hover:shadow-2xl transition duration-300">
+              <a href="#" class="p-2 border border-gray-500 bg-white rounded-xl shadow-md hover:shadow-xl transition duration-300">
                 <img class="rounded-xl" src="https://www.saturnme.com/wp-content/uploads/2019/10/vertical-logo-monochromatic-1.png" alt="" />
               </a>
 
-              <a href="#" class="p-2 bg-white rounded-xl shadow-md hover:shadow-2xl transition duration-300">
+              <a href="#" class="p-2 border border-gray-500 bg-white rounded-xl shadow-md hover:shadow-xl transition duration-300">
                 <img class="rounded-xl" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpdQS9SPTGck-LiPrdohgvVojOzPFXxbnhGEfhpnk1q1SRQKhlibXt22OG4Nmejb3IEdk&usqp=CAU" alt="" />
               </a>
 
-              <a href="#" class="p-2 bg-white rounded-xl shadow-md hover:shadow-2xl transition duration-300">
+              <a href="#" class="p-2 border border-gray-500 bg-white rounded-xl shadow-md hover:shadow-xl transition duration-300">
                 <img class="rounded-xl" src="https://lh3.googleusercontent.com/proxy/-3A9rNjGl3Hg12qqZKJZL6rGEa-THENUi1am3upNa5d-HKh3GtD3BkCZwnQyyYOFPKk3KLWo_R-BRdiQSsF_wYb2VPJD2eJJoRhGs0KJbFFb3A" alt="" />
               </a>
 
-              <a href="#" class="p-2 bg-white rounded-xl shadow-md hover:shadow-2xl transition duration-300">
+              <a href="#" class="p-2 border border-gray-500 bg-white rounded-xl shadow-md hover:shadow-xl transition duration-300">
                 <img class="rounded-xl" src="https://seekvectorlogo.com/wp-content/uploads/2018/11/plivo-inc-vector-logo-small.png" alt="" />
               </a>
 
 
    
-              <a href="#" class="p-2 bg-white rounded-xl shadow-md hover:shadow-2xl transition duration-300">
+              <a href="#" class="p-2 border border-gray-500 bg-white rounded-xl shadow-md hover:shadow-xl transition duration-300">
                 <img class="rounded-xl" src="https://images.squarespace-cdn.com/content/v1/5e1c37cf9b245f360f07ee55/1620883887546-H8C6WEA322YT6DUZJ4TL/EmailMidImage_500x500_A+-+Shopper%402x.png" alt="" />
               </a>
 
-              <a href="#" class="p-2 bg-white rounded-xl shadow-md hover:shadow-2xl transition duration-300">
+              <a href="#" class="p-2 border border-gray-500 bg-white rounded-xl shadow-md hover:shadow-xl transition duration-300">
                 <img class="rounded-xl" src="https://www.cardpaymentoptions.com/wp-content/uploads/2020/11/stripe-purple.jpg" alt="" />
               </a>
 
-              <a href="#" class="p-2 bg-white rounded-xl shadow-md hover:shadow-2xl transition duration-300">
+              <a href="#" class="p-2 border border-gray-500 bg-white rounded-xl shadow-md hover:shadow-xl transition duration-300">
                 <img class="rounded-xl" src="https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,f_auto,q_auto:eco,dpr_1/xli63lzqaz7zgbsgiu5h" alt="" />
               </a>
 
